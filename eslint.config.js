574a3e2f8bb94e88a03b0dc593030efe57133ebf
Mohamed Atabay <mohamed.atabay@outlook.com>
2025-10-09 21:11:35 +0200

    Added textarea, converted js to ts, added mui

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,4 @@
-import js from '@eslint/js'
+import ts from '@eslint/ts'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
@@ -7,9 +7,9 @@ import { defineConfig, globalIgnores } from 'eslint/config'
 export default defineConfig([
   globalIgnores(['dist']),
   {
-    files: ['**/*.{js,jsx}'],
+    files: ['**/*.{ts,tsx}'],
     extends: [
-      js.configs.recommended,
+      ts.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
     ],
@@ -18,7 +18,7 @@ export default defineConfig([
       globals: globals.browser,
       parserOptions: {
         ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
+        ecmaFeatures: { tsx: true },
         sourceType: 'module',
       },
     },
