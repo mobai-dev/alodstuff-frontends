217d3fe771e16adc44cf03a46bac3d4c713b837f
Mohamed Atabay <mohamed.atabay@outlook.com>
2025-10-11 22:38:20 +0200

    Added first functioning textarea with unicode symbol translation

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import TextEditor from './components/TextEditor/TextEditor';
+import ControlPane from './components/ControlPane/ControlPane';
 
 function App() {
   return (
-    <>
-      <TextEditor />
-    </>
+    <div className='w-full h-full'>
+      <ControlPane/>
+      <TextEditor/>
+    </div>
   )
 }
 
--- src/components/TextEditor/TextEditor.tsx
+++ src/components/TextEditor/TextEditor.tsx
@@ -1,12 +1,43 @@
-import { TextareaAutosize } from "@mui/material"
 import './TextEditor.css';
+import { useState } from 'react';
 
 function TextEditor() {
+    const [text, setText] = useState("");
+
+    const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+        let input = event.target.value;
+
+        // TODO: put this somewhere else. maybe make it configurable in a json or so
+        const replacements: Record<string, string> = {
+            "--": "\u2012",
+            ".-": "\u2022",
+            "...": "\u2026",
+            "<-": "\u27F5",
+            "->": "\u27F6",
+            "<>": "\u27F7",
+            "!!": "\u2757"
+        }
+
+        for(const [symbol, replacement] of Object.entries(replacements)) {
+            const regex = new RegExp(symbol.replace(/[.*+?^${}()|[\]\\]/g, "\\$&"), "g");
+            input = input.replace(regex, replacement);
+        }
+
+        setText(input);
+    }
+
     return (
-        <TextareaAutosize
-            className={ 'h100' }
-        />
+        <textarea 
+            placeholder='Type something...'
+            value={text}
+            onChange={handleChange} />
     )
 }
+export default TextEditor;
 
-export default TextEditor
+// TODO:
+// bold, italic, normal
+// colors 
+// font sizes
+
+// clear button, save button, save locally, import?
